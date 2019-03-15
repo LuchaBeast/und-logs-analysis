de8c1f91ba841a577a77be01dab834561245f83c
Drew Shamrock <drewshamrock@gmail.com>
2019-03-15 13:34:16 -0700

    Renamed function to get_popular_articles(), updated the SQL query to use the popular_articles view to retrieve records, cleaned up comments

--- logs-analysis.py
+++ logs-analysis.py
@@ -1,14 +1,18 @@
-import psycopg2 #import the psycopg2 module for running PostgreSQL queries
+# Import the psycopg2 module for running PostgreSQL queries.
+import psycopg2
 
-def get_reports():
-    db = psycopg2.connect("dbname=news") #connect to news database
-    cur = db.cursor() #create a new cursor from database
+# Function to retrieve 3 most popular articles from database.
+def get_popular_articles():
+    
+    # Connect to news database and create a new cursor.
+    db = psycopg2.connect("dbname=news")
+    cur = db.cursor()
 
-    cur.execute("SELECT * FROM authors;")
+    # Query the popular_articles view to retrieve 3 most popular queries.
+    cur.execute("SELECT * FROM popular_articles;")
 
+    # Return the most popular articles and close connection to database.
     return cur.fetchall()
     db.close()
 
-for i in range(len(get_reports())):
-    print(get_reports()[i][0])
-    print("\n")
+print(get_popular_articles())
