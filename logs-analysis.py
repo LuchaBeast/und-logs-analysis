2da7ef1e22009050d35deb1873cfc7f361dcffd2
vagrant <vagrant@vagrant.vm>
2019-03-15 18:59:06 +0000

    Iterate over get_reports() and print out each record

--- logs-analysis.py
+++ logs-analysis.py
@@ -4,9 +4,11 @@ def get_reports():
     db = psycopg2.connect("dbname=news") #connect to news database
     cur = db.cursor() #create a new cursor from database
 
-    cur.execute("SELECT * FROM authors LIMIT 2;")
+    cur.execute("SELECT * FROM authors;")
 
     return cur.fetchall()
     db.close()
 
-print(get_reports())
+for i in range(len(get_reports())):
+    print(get_reports()[i][0])
+    print("\n")
