44576b06f331c298eff180edfc6f45a0aa97e4cc
Drew Shamrock <drewshamrock@gmail.com>
2019-03-19 08:58:47 -0700

    Nest individual report functions under retrieve_reports() function and begin printing out results, starting with Most Popular Articles.

--- logs-analysis.py
+++ logs-analysis.py
@@ -1,47 +1,49 @@
 # Import the psycopg2 module for running PostgreSQL queries.
 import psycopg2
 
-# Function to retrieve 3 most popular articles from database.
-def get_popular_articles():
-    
-    # Connect to news database and create a new cursor.
-    db = psycopg2.connect("dbname=news")
-    cur = db.cursor()
+def retrieve_reports():
+    # Function to retrieve 3 most popular articles from database.
+    def get_popular_articles():
+        
+        # Connect to news database and create a new cursor.
+        db = psycopg2.connect("dbname=news")
+        cur = db.cursor()
 
-    # Query the popular_articles view to retrieve 3 most popular queries.
-    cur.execute("SELECT * FROM popular_articles;")
+        # Query the popular_articles view to retrieve 3 most popular queries.
+        cur.execute("SELECT * FROM popular_articles;")
+        
+        # Return the most popular articles and close connection to database.
+        return cur.fetchall()
+        db.close()
 
-    # Return the most popular articles and close connection to database.
-    return cur.fetchall()
-    db.close()
+    # Function to retrieve 3 most popular authors from database.
+    def get_popular_authors():
+        # Connect to news database and create a new cursor.
+        db = psycopg2.connect("dbname=news")
+        cur = db.cursor()
 
-# Function to retrieve 3 most popular authors from database.
-def get_popular_authors():
-    # Connect to news database and create a new cursor.
-    db = psycopg2.connect("dbname=news")
-    cur = db.cursor()
+        # Query the popular_authors view to retrieve 3 most popular authors.
+        cur.execute("SELECT * FROM popular_authors;")
 
-    # Query the popular_authors view to retrieve 3 most popular authors.
-    cur.execute("SELECT * FROM popular_authors;")
+        # Return the most popular authors and close the connection to the database.
+        return cur.fetchall()
+        db.close()
 
-    # Return the most popular authors and close the connection to the database.
-    return cur.fetchall()
-    db.close()
+    # Function to retrieve days where error rates were >1%.
+    def get_error_rates():
+        # Connect to news database and create a new cursor.
+        db = psycopg2.connect("dbname=news")
+        cur = db.cursor()
 
-# Function to retrieve days where error rates were >1%.
-def get_error_rates():
-    # Connect to news database and create a new cursor.
-    db = psycopg2.connect("dbname=news")
-    cur = db.cursor()
+        # Query the error_rates view to retrieve days with >1% error rates.
+        cur.execute("SELECT * FROM error_rates;")
 
-    # Query the error_rates view to retrieve days with >1% error rates.
-    cur.execute("SELECT * FROM error_rates;")
+        # Return the days with >1% error rates and close the connection to the database.
+        return cur.fetchall()
+        db.close()
 
-    # Return the days with >1% error rates and close the connection to the database.
-    return cur.fetchall()
-    db.close()
+    print("The most popular articles are:")
+    for i in range(len(get_popular_articles())):
+        print('*',get_popular_articles()[i][0], '-', get_popular_articles()[i][1])
 
-
-print(get_popular_articles())
-print(get_popular_authors())
-print(get_error_rates())
+retrieve_reports()
