dd560c952243e35bf945d2f06d88b6ca9ed9a378
Drew Shamrock <drewshamrock@gmail.com>
2019-03-19 09:59:55 -0700

    Fix styling to be compliant with PEP 8 and add shebang to beginning of file.

--- logs-analysis.py
+++ logs-analysis.py
@@ -1,24 +1,28 @@
+#!/usr/bin/python3
+
 # Import the psycopg2 module for running PostgreSQL queries.
 import psycopg2
+
 
 # Function to retrieve all 3 reports
 def retrieve_reports():
     # Function to retrieve 3 most popular articles from database.
     def get_popular_articles():
-        
+
         # Connect to news database and create a new cursor.
         db = psycopg2.connect("dbname=news")
         cur = db.cursor()
 
         # Query the popular_articles view to retrieve 3 most popular queries.
         cur.execute("SELECT * FROM popular_articles;")
-        
+
         # Return the most popular articles and close connection to database.
         return cur.fetchall()
         db.close()
 
     # Function to retrieve 3 most popular authors from database.
     def get_popular_authors():
+
         # Connect to news database and create a new cursor.
         db = psycopg2.connect("dbname=news")
         cur = db.cursor()
@@ -26,12 +30,13 @@ def retrieve_reports():
         # Query the popular_authors view to retrieve 3 most popular authors.
         cur.execute("SELECT * FROM popular_authors;")
 
-        # Return the most popular authors and close the connection to the database.
+        # Return most popular authors and close the connection to the database.
         return cur.fetchall()
         db.close()
 
     # Function to retrieve days where error rates were >1%.
     def get_error_rates():
+
         # Connect to news database and create a new cursor.
         db = psycopg2.connect("dbname=news")
         cur = db.cursor()
@@ -39,27 +44,34 @@ def retrieve_reports():
         # Query the error_rates view to retrieve days with >1% error rates.
         cur.execute("SELECT * FROM error_rates;")
 
-        # Return the days with >1% error rates and close the connection to the database.
+        # Return days with >1% errors and close the connection to the database.
         return cur.fetchall()
         db.close()
 
     # Iterate over get_popular_articles() list and print out results
     print('The most popular articles are:')
     for i in range(len(get_popular_articles())):
-        print('*',get_popular_articles()[i][0], '-', get_popular_articles()[i][1])
+        print(
+            '*', get_popular_articles()[i][0],
+            '-', get_popular_articles()[i][1])
 
     print('\n')
 
     # Iterate over get_popular_authors() list and print out results
     print('The most popular authors are:')
     for i in range(len(get_popular_authors())):
-        print('*', get_popular_authors()[i][0], '-', get_popular_authors()[i][1])
+        print(
+            '*', get_popular_authors()[i][0],
+            '-', get_popular_authors()[i][1])
 
     print('\n')
 
     # Iterate over get_error_rates() list and print out results
     print('Days with error rates >1%:')
     for i in range(len(get_error_rates())):
-        print('*', get_error_rates()[i][0], '-', get_error_rates()[i][1])
+        print(
+            '*', get_error_rates()[i][0],
+            '-', get_error_rates()[i][1])
+
 
 retrieve_reports()
